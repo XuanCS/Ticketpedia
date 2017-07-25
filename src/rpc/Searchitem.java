053e55d373a97a26bdeff0828567ecce6adbafbf
Xuan Lu <xuanlu@iastate.edu>
2017-07-25 18:35:12 -0500

    First commit.

---
+++ src/rpc/Searchitem.java
@@ -0,0 +1,143 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
+import external.ExternalAPI;
+import external.ExternalAPIFactory;
+
+/**
+ * Servlet implementation class Searchitem
+ */
+@WebServlet("/search")
+public class Searchitem extends HttpServlet {
+	private DBConnection conn = DBConnectionFactory.getDBConnection();
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Searchitem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+//		response.setContentType("application/json");
+//		response.addHeader("Access-Control-Allow-Origin", "*");
+//		String username = "";
+//		PrintWriter out = response.getWriter();
+//		if (request.getParameter("username") != null) {
+//			username = request.getParameter("username");
+//			out.print("Hello " + username);
+//		}
+//		out.flush();
+//		out.close();
+		
+		
+//		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+//		out.println("<html><body>");
+//		out.println("<h1>This is a HTML page</h1>");
+//		out.println("</body></html>");
+//		out.flush();
+//		out.close();
+		
+		
+//		response.setContentType("application/json");
+//		response.addHeader("Access-Control-Allow-Origin", "*");
+//
+//		String username = "";
+//		if (request.getParameter("username") != null) {
+//			username = request.getParameter("username");
+//		}
+//		JSONObject obj = new JSONObject();
+//		try {
+//			obj.put("username", username);
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+//		PrintWriter out = response.getWriter();
+//		out.print(obj);
+//		out.flush();
+//		out.close();
+		
+//		JSONArray array = new JSONArray();
+//		try {
+//			array.put(new JSONObject().put("username", "abcd"));
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+//		RpcHelper.writeJsonArray(response, array);
+		
+//		double lat = Double.parseDouble(request.getParameter("lat"));
+//		double lon = Double.parseDouble(request.getParameter("lon"));
+//		// Term can be empty or null.
+//		String term = request.getParameter("term");
+//		ExternalAPI externalAPI = ExternalAPIFactory.getExternalAPI();
+//		List<Item> items = externalAPI.search(lat, lon, term);
+//		List<JSONObject> list = new ArrayList<>();
+//		try {
+//			for (Item item : items) {
+//				// Add a thin version of item object
+//				JSONObject obj = item.toJSONObject();
+//				list.add(obj);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		JSONArray array = new JSONArray(list);
+//		RpcHelper.writeJsonArray(response, array);
+		
+		String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		// Term can be empty or null.
+		String term = request.getParameter("term");
+		List<Item> items = conn.searchItems(userId, lat, lon, term);
+		List<JSONObject> list = new ArrayList<>();
+		try {
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				list.add(obj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray array = new JSONArray(list);
+		RpcHelper.writeJsonArray(response, array);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
